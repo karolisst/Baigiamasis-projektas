4864bb4a3b2ca2a12aea8b552a5ad3dc191ccdef
karolis steponavicius <karolis.stpn@gmail.com>
2023-05-22 19:55:26 +0300

    added - homepage, protected; changed - navbar

--- frontend/administration-app/src/App.js
+++ frontend/administration-app/src/App.js
@@ -1,16 +1,35 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { AuthenticationContext } from "./components/AuthenticationContext";
 import { Login } from "./components/Login";
 import { Register } from "./components/Register";
 import { NavBar } from "./components/NavBar";
+import Protected from "./components/Protected";
+import { HomePage } from "./components/HomePage";
 
 function App() {
+  const { setIsSignedIn } = useContext(AuthenticationContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsSignedIn(false);
+  };
+
   return (
     <>
-      <NavBar />
+      <NavBar onLogout={handleLogout} />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route
+          element={
+            <Protected isLoading={isLoading} setIsLoading={setIsLoading} />
+          }
+        >
+          <Route path="/" element={<HomePage />} />
+        </Route>
       </Routes>
     </>
   );
