ab72752df1afd71c13aed1a35d2aa6ecb1103ef8
karolis steponavicius <karolis.stpn@gmail.com>
2023-05-23 16:54:45 +0300

    added user table routers

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const authenticationRouter = require("./routes/authentication");
+const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(authenticationRouter);
+app.use(usersRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ backend/routes/users.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const { dbConnection } = require("../db");
+const { defaultCallback } = require("../utils/dbUtils");
+
+const router = express.Router();
+
+router.get("/users", (req, res) => {
+  dbConnection.execute("SELECT * FROM users", (err, result) =>
+    defaultCallback(err, result, res)
+  );
+});
+
+router.post("/users", (req, res) => {
+  const { name, surname, email, phone_number } = req.body;
+
+  dbConnection.execute(
+    `INSERT INTO users (name, surname, email, phone_number)
+      VALUES (?, ?, ?, ?)
+      `,
+    [name, surname, email, phone_number],
+    (err, result) => defaultCallback(err, result, res)
+  );
+});
+
+router.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  dbConnection.execute("DELETE FROM users WHERE id=?", [id], (err, result) =>
+    defaultCallback(err, result, res)
+  );
+});
+
+module.exports = router;
